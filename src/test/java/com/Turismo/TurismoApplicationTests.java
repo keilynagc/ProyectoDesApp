b55691509d9ede7b4965b34977711a675ce858f9
Keilyn Aguero C <90811028+keilynagc@users.noreply.github.com>
2023-07-12 20:57:37 -0600

    Avance 2

---
+++ src/test/java/com/Turismo/TurismoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Turismo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurismoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
